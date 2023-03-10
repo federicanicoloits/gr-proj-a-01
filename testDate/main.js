54d44f61d5652f2ac7a84df94ce2d7e5ec23c3ae
umbertopaolasso <117468539+umbertopaolasso@users.noreply.github.com>
2023-03-10 17:34:15 +0100

    Add files via upload

---
+++ testDate/main.js
@@ -0,0 +1,27 @@
+// prende la data attuale e restituisce un oggetto, cosi ci possiamo lavorare sopra
+let formatDate = () => {
+  const date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  let day = date.getDate();
+  
+  // creazione del oggetto date personalizzato
+  let dataObject = {'day':day, 'month': month, 'year': year};
+  return dataObject;
+}
+
+// prende un oggetto ritorna una stringa
+let printDate = (objectDate) => {
+  let unionDate = (objectDate.day + '-' + objectDate.month + '-' + objectDate.year);
+  return unionDate;
+}
+
+
+
+let e = formatDate()
+//ritorna una oggetto, cosi se vogliamo andare a fare i controlli ad es con il mese attuale - scadedenza si può
+console.log(typeof(e));
+console.log(e);
+// 
+
+console.log(printDate(e));
---
+++ testDate/readme.md
@@ -0,0 +1,11 @@
+# js-08-the-dom
+## Author
+Author: Paolasso Umberto
+Contact: umberto.paolasso@edu.itspiemonte.it
+Role: Frontend Developer
+***
+## Excercise requirement
+
+ 
+***
+## Approach to solution
