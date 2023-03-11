81f90fd1c25cc8a07c47dbd527c53980f1f22747
federicanicoloits <127236809+federicanicoloits@users.noreply.github.com>
2023-03-11 11:07:05 +0000

    federica2

--- scripts/federica.js
+++ scripts/federica.js
@@ -31,6 +31,53 @@ function generateID(){
   }
   return ID;
 }
+export {generateID, initialControl}
+/**
+ * The function takes an initialConfig object as a parameter and checks each value of the object to ensure it is a number greater than 0 and returns a modified object
+ * @param {object} initialConfig - object that contain the initial configuration
+ */
+function initialControl(initialConfig){
+  // Initialize an empty array for control values
+  let control = [];
+
+  // Iterate through each key in the initialConfig object
+  for(let i=0; i<Object.keys(initialConfig).length; i++){
+    // Store the current value in the i-th position of the control array
+    control[i] = initialConfig[Object.keys(initialConfig)[i]];
+
+    // Check if the current value is defined
+    if (control[i] != undefined){
+      // Check if the current value is not null, Infinity, or -Infinity
+      if (control[i] != null && control[i] != Infinity && control[i] != - Infinity){
+        // Check if the current value is a number
+        if (!isNaN(control[i])){
+          // Check if the current value is greater than 0
+          if(control[i]>0){
+            // Convert the current value to an integer and log a message
+            initialConfig[Object.keys(initialConfig)[i]] = parseInt(control[i], 10);
+            console.log("The number will be set to " + initialConfig[Object.keys(initialConfig)[i]]);
+          } else {
+            // Log an error message and set the current value to 1
+            console.error("Error! Entered a number minor of 0. The number will be set to 1");
+            initialConfig[Object.keys(initialConfig)[i]] = 1;
+          }
+        } else {
+          // Log an error message and set the current value to 1
+          console.error("Error! Entered not a number. The number will be set to 1");
+          initialConfig[Object.keys(initialConfig)[i]] = 1;
+        }
+      } else {
+        // Log an error message and set the current value to 1
+        console.error("Error! You have entered value not permitted. The number will be set to 1.");
+        initialConfig[Object.keys(initialConfig)[i]] = 1;
+      }
+    } else {
+      // Log an error message and set the current value to 1
+      console.error("Error! You have not entered a value. The number will be set to 1.");
+      initialConfig[Object.keys(initialConfig)[i]] = 1;
+    }
+  }
+}
 //Initial Configuration
 alert("Here you can enter the initial configuration. Open the console to see the results");
 let numb1 = prompt("STEP 1 - Write a number from 1 to 6 to indicate after how many weeks the products will have to be removed", "1");
